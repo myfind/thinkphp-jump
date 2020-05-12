e766e3aa3ed678a838f1f62fa5aec1d2177b5839
myfind <364998473@qq.com>
2020-05-12 21:41:48 +0800

    Add files via upload

---
+++ src/config/jump.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: myfind
+ */
+return[
+    // 默认跳转页面对应的模板文件
+    'dispatch_success_tmpl' => app()->getRootPath().'/vendor/myfind/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/myfind/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+];
